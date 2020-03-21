da01e5757734f61ba44e3252ef0dde1bc67651c3
向野佑基 <konoyuki@KonoyukiBook.local>
2020-03-22 01:42:13 +0900

    5

--- src/contact-data.js
+++
@@ -1,9 +0,0 @@
-app.formdata = {
-    doc: "https://docs.google.com/forms/u/0/d/e/1FAIpQLSd7ijE551XtnN__81ixUDyouLW3nHR_IN5u_QS9tYq3kKsfiQ/formResponse",
-    survey: [
-        {
-            name: "entry.2005620554",
-            
-        }
-    ]
-}
